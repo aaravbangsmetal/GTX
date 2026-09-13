0e207ca6364191bd8e7eeb54db7d1d0295cc0160
surfersbangs <surfers.boobs@gmail.com>
2026-09-14 04:54:04 +0530

    Export core module public API.

---
+++ src/core/index.ts
@@ -0,0 +1,9 @@
+export { AssetLoader } from './AssetLoader';
+export { createCoreSystem } from './createSystem';
+export { DevOverlay } from './DevOverlay';
+export { EntityManager } from './EntityManager';
+export { Game } from './Game';
+export { GameLoop } from './GameLoop';
+export { StateManager } from './StateManager';
+export { SystemRegistry } from './SystemRegistry';
+export { createStubSystem, STUB_SYSTEM_NAMES } from './stubs';
